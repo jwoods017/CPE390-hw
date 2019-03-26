dbf4ee5977491c11ab3e5874a28dc8e10b025815
jwoods017 <jwoods@github.com>
2019-03-25 20:29:57 -0400

    hw5 submission

--- hw5power/power.s
+++ hw5power/power.s
@@ -3,27 +3,34 @@
   Cite: n/a
   I pledge my honor that I have
   abided by the Stevens Honor System
+	
+	R0 - BASE
+	R1 - EXPONENT/COUNTER
+	R3 - PRODUCT
 */
+	
+	.globl _Z5powerjj
+_Z5powerjj:	
+	
+	PUSH {R2}
+	MOV R3, R0 	//MAKES PRODUCT EQUAL TO BASE 
 
-     /*
-     R0 - NUMBER TO BE MULTIPLIED
-     R1 - NUMBER OF TIMES TO MULTIPLY
-     R2 - PRODUCT
-     */
-
-     PUSH {R1, R2}
-     LDR R2, R0
-     
 .loop:
+
+	CMP R1, #1	//checks if exponent is 1
+	BEQ .leave	//branches to exit function
+
+	MUL R3, R0	//MULTIPLIES PRODUCT BY BASE
+	SUB R1, #1	//DROPS EXPONENT/COUNTER BY 1
+
+	BGT .loop	//LOOPS BACK
+
+.leave:
+	MOV R0, R3	//SETS R0 TO PRODUCT
+	POP {R2}
+	BX LR		//RETURNS
      
-     CMP R1, #1
-     BEQ .exit
-     MUL R2, R0
-     SUB R1, #1
-     CMP R1, #1
-     BGT .loop
-     BX LR
      
-.exit:
-     LDR R0, R1
-     BX LR
+
+	
+	
